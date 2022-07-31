e7827cc1b25a5d0e99d86ec5f561b4a3137bcace
DimaChekashov <foxdima99@gmail.com>
2022-07-31 15:36:12 +0300

    test commit

--- fox-say-portfolio/src/components/Greeting/Greeting.tsx
+++ fox-say-portfolio/src/components/Greeting/Greeting.tsx
@@ -13,7 +13,7 @@ const Greeting: React.FC<GreetingProps> = () => {
         Intro text: Lorem ipsum dolor sit amet, 
         consectetur adipiscing elit, sed do 
         eiusmod tempor incididunt ut labore 
-        et dolore magna aliqua. 
+        et dolore magna aliqua. aaa
       </div>
       <button type="button">
         Let’s get started
