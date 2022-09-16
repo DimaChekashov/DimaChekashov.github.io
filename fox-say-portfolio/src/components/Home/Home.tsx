ba5661ba541104df7f505fbf1849d8825611839d
DimaChekashov <foxdima99@gmail.com>
2022-09-16 17:39:46 +0300

    connect particulars js

--- fox-say-portfolio/src/components/Home/Home.tsx
+++ fox-say-portfolio/src/components/Home/Home.tsx
@@ -1,32 +1,33 @@
 import React from "react";
-import { HomeProps } from "./Home.props";
+import {HomeProps} from "./Home.props";
 import FoxSayImg from "./../../assets/foxsay.jpg";
 import styles from "./Home.module.scss";
+import ParticlesBackground from "../ParticlesBackground/ParticlesBackground";
 
 const Home: React.FC<HomeProps> = () => {
-    return (
-        <div className="home">
-            <h1>Dmitry Chekashov</h1>
-            <div>
-                Intro text: Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. aaa
-            </div>
-            <button type="button">Let’s get started</button>
-            <img src={FoxSayImg} alt="" />
-            <div>
-                <a href="#">
-                    <img src="#" alt="Telegram" />
-                </a>
-                <a href="#">
-                    <img src="#" alt="Habr Career" />
-                </a>
-                <a href="#">
-                    <img src="#" alt="Linkedin" />
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.home}>
+      <ParticlesBackground dark={true} />
+      <h1>Dmitry Chekashov</h1>
+      <div>
+        Intro text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. aaa
+      </div>
+      <button type="button">Let’s get started</button>
+      <img src={FoxSayImg} alt="" />
+      <div>
+        <a href="#">
+          <img src="#" alt="Telegram" />
+        </a>
+        <a href="#">
+          <img src="#" alt="Habr Career" />
+        </a>
+        <a href="#">
+          <img src="#" alt="Linkedin" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ fox-say-portfolio/src/components/ParticlesBackground/ParticlesBackground.tsx
@@ -0,0 +1,155 @@
+import React, {useCallback} from "react";
+import Particles from "react-tsparticles";
+import type {Engine, Container} from "tsparticles-engine";
+import {loadFull} from "tsparticles";
+
+import styles from "./ParticlesBackground.module.scss";
+
+enum colors {
+  darkTheme = "#bc6ff1",
+  lightTheme = "#3f72af"
+}
+
+interface Props {
+  dark: boolean;
+}
+
+const ParticlesBackground: React.FC<Props> = ({dark}) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: Container | undefined) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      className={styles.particles}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+          enable: false,
+          zIndex: 1
+        },
+        background: {
+          color: {
+            value: "var(--background-color)"
+          }
+        },
+        fpsLimit: 120,
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: {
+              enable: false,
+              mode: "bubble"
+            },
+            onclick: {
+              enable: false
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 215.7842157842158,
+              line_linked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 250,
+              size: 0,
+              duration: 2,
+              opacity: 0,
+              speed: 3
+            },
+            repulse: {
+              distance: 400,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        particles: {
+          number: {
+            value: 160,
+            density: {
+              enable: true,
+              value_area: 800
+            }
+          },
+          color: {
+            value: !dark ? colors.darkTheme : colors.lightTheme
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: !dark ? colors.lightTheme : colors.darkTheme
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "img/github.svg",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 1,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0,
+              sync: false
+            }
+          },
+          size: {
+            value: 15.782952832645451,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 4,
+              size_min: 0.3,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: false,
+            distance: 150,
+            color: !dark ? colors.darkTheme : colors.lightTheme,
+            opacity: 0.4,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 600
+            }
+          }
+        },
+        detectRetina: true
+      }}
+    />
+  );
+};
+
+export default ParticlesBackground;
