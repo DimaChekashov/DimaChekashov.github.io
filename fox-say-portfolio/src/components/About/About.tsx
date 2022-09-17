e174aa6c71ea94d91c0890e15d81f7844c0e6a1f
DimaChekashov <foxdima99@gmail.com>
2022-09-17 18:50:41 +0300

    fill about

--- fox-say-portfolio/src/components/About/About.tsx
+++ fox-say-portfolio/src/components/About/About.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { AboutProps } from "./About.props";
+import {AboutProps} from "./About.props";
 import styles from "./About.module.scss";
 
 const About: React.FC<AboutProps> = () => {
-    return <div className="about">About</div>;
+  return (
+    <div className={styles.about}>
+      <h2 className={styles.aboutTitle}>About me</h2>
+      <div className={styles.aboutDesc}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
+        suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
+        harum.
+      </div>
+      <div className={styles.aboutDesc}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
+        suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
+        harum.
+      </div>
+      <div className={styles.aboutDesc}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
+        suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
+        harum.
+      </div>
+      <div className={styles.aboutLink}>Lets do something!</div>
+    </div>
+  );
 };
 
 export default About;
