80b135a880bb810f7c578f3cdce1e788a3e52399
DimaChekashov <foxdima99@gmail.com>
2022-11-03 02:14:17 +0300

    update text

--- fox-say-portfolio/src/components/About/About.tsx
+++ fox-say-portfolio/src/components/About/About.tsx
@@ -5,23 +5,23 @@ import styles from "./About.module.scss";
 const About: React.FC<AboutProps> = () => {
   return (
     <div className={styles.about}>
-      <h2 className={styles.aboutTitle}>About me</h2>
-      <div className={styles.aboutDesc}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
-        suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
-        harum.
+      <div className={styles.aboutBlock}>
+        <h2 className={styles.aboutTitle}>My, Myself & I</h2>
+        <div className={styles.aboutDesc}>
+          A frontend developer with 4 years of extensive experience in Web Development.
+        </div>
+        <div className={styles.aboutDesc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
+          suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
+          harum.
+        </div>
+        <div className={styles.aboutDesc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
+          suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
+          harum.
+        </div>
+        <div className={styles.aboutLink}>Lets do something!</div>
       </div>
-      <div className={styles.aboutDesc}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
-        suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
-        harum.
-      </div>
-      <div className={styles.aboutDesc}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A dolorem nobis provident adipisci, temporibus odit
-        suscipit. Culpa nemo cupiditate natus, quibusdam dolorem quod labore. Voluptas exercitationem labore fuga magnam
-        harum.
-      </div>
-      <div className={styles.aboutLink}>Lets do something!</div>
     </div>
   );
 };
--- fox-say-portfolio/src/components/Skills/Skills.tsx
+++ fox-say-portfolio/src/components/Skills/Skills.tsx
@@ -5,32 +5,53 @@ import styles from "./Skills.module.scss";
 const Skills: React.FC<SkillsProps> = () => {
   return (
     <div className={styles.skills}>
-      <h2 className={styles.skillsTitle}>Skills & Experience</h2>
-      <div className={styles.skillsDesc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quae corrupti adipisci libero non minus omnis
-        similique, veniam beatae, dignissimos repellendus minima harum necessitatibus blanditiis. Excepturi vero
-        corporis adipisci earum.
-      </div>
-      <div className={styles.skillsDesc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quae corrupti adipisci libero non minus omnis
-        similique, veniam beatae, dignissimos repellendus minima harum necessitatibus blanditiis. Excepturi vero
-        corporis adipisci earum.
+      <div className={styles.skillsBlock}>
+        <h2 className={styles.skillsTitle}>Skills & Experience</h2>
+        <div className={styles.skillsDesc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quae corrupti adipisci libero non minus omnis
+          similique, veniam beatae, dignissimos repellendus minima harum necessitatibus blanditiis. Excepturi vero
+          corporis adipisci earum.
+        </div>
+        <div className={styles.skillsDesc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quae corrupti adipisci libero non minus omnis
+          similique, veniam beatae, dignissimos repellendus minima harum necessitatibus blanditiis. Excepturi vero
+          corporis adipisci earum.
+        </div>
       </div>
       <div className={styles.companies}>
         <div className={styles.company}>
-          <h3 className={styles.companyTitle}>Company Name</h3>
-          <div className={styles.companyDate}>09.09.2021 - 09.09.2022</div>
-          <div className={styles.companyDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+          <h3 className={styles.companyTitle}>Rebrain - <span>Frontend Developer</span></h3>
+          <div className={styles.companyLocation}>Moscow, Russia</div>
+          <div className={styles.companyDate}>Jun 2021 - Present</div>
+          <ul className={styles.companyList}>
+            <li>Engaged in the development and support of web pages of the site</li>
+            <li>Develop Landing Pages</li>
+            <li>Engaged in cross-browser adaptive layout of web components</li>
+            <li>Participated in task planning and evaluation</li>
+            <li>Participated in interface design</li>
+          </ul>
         </div>
         <div className={styles.company}>
-          <h3 className={styles.companyTitle}>Company Name</h3>
-          <div className={styles.companyDate}>09.09.2021 - 09.09.2022</div>
-          <div className={styles.companyDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+          <h3 className={styles.companyTitle}>Community Phone - <span>Frontend Developer</span></h3>
+          <div className={styles.companyLocation}>Remote, Boston, USA</div>
+          <div className={styles.companyDate}>Mar 2022 - Aug 2022</div>
+          <ul className={styles.companyList}>
+            <li>Development and support of the service on React</li>
+            <li>Engaged in cross-browser adaptive layout of web components</li>
+          </ul>
         </div>
         <div className={styles.company}>
-          <h3 className={styles.companyTitle}>Company Name</h3>
-          <div className={styles.companyDate}>09.09.2021 - 09.09.2022</div>
-          <div className={styles.companyDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+          <h3 className={styles.companyTitle}>It Chelny - <span>Frontend Developer</span></h3>
+          <div className={styles.companyLocation}>Naberezhnye Chelny, Russia</div>
+          <div className={styles.companyDate}>Mar 2017 - Nov 2019 | Jan 2021 - Jun 2021</div>
+          <ul className={styles.companyList}>
+            <li>Engaged in development and support of web pages</li>
+            <li>Develop Landing Pages, Online Stores, Portals, News Sites</li>
+            <li>Adapted existing sites for mobile devices</li>
+            <li>Optimized and accelerated site loading using Lighthouse</li>
+            <li>Participated in the planning and evaluation of tasks</li>
+            <li>Develop Javascript widgets</li>
+          </ul>
         </div>
       </div>
       <div className={styles.technology}>
