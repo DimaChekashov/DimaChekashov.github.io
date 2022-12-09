092bfe795fe4d2e750eb4dc1f169bc5ec86778f1
DimaChekashov <foxdima99@gmail.com>
2022-12-09 21:23:53 +0300

    try to connect smtp

--- fox-say-portfolio/src/pages/Contact/Contact.tsx
+++ fox-say-portfolio/src/pages/Contact/Contact.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import {motion} from "framer-motion";
 import styles from "./Contact.module.scss";
 import Loader from "../../components/Loader/Loader";
+import { SMTPClient } from "emailjs";
+
+const client = new SMTPClient({
+  user: "foxdima99@gmail.com",
+  password: "",
+  host: "smtp.gmail.com",
+  ssl: true
+});
 
 const Contact: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [subject, setSubject] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const sendEmail = () => {
+    client.send({
+      text: 'i hope this works',
+      from: 'you <username@your-email.com>',
+      to: 'someone <someone@your-email.com>, another <another@your-email.com>',
+      cc: 'else <else@your-email.com>',
+      subject: 'testing emailjs',
+    }, (err, message) => {
+      console.log(err || message);
+    })
+    clearFields();
+  }
+
+  const clearFields = () => {
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  }
+
   return (
     <>
       <motion.div 
@@ -15,11 +48,38 @@ const Contact: React.FC = () => {
       >
         <h2 className={styles.contactTitle}>Contact me</h2>
         <form className={styles.contactForm}>
-          <input type="text" className={styles.contactInput} placeholder="Name" />
-          <input type="text" className={styles.contactInput} placeholder="Email" />
-          <input type="text" className={styles.contactInput} placeholder="Subject" />
-          <textarea className={styles.contactTextarea} placeholder="Message" />
-          <button type="submit" className={styles.contactBtn}>
+          <input 
+            type="text" 
+            className={styles.contactInput} 
+            placeholder="Name" 
+            value={name} 
+            onChange={(e: React.FormEvent<HTMLInputElement>) => setName(e.currentTarget.value)} 
+          />
+          <input 
+            type="text" 
+            className={styles.contactInput} 
+            placeholder="Email" 
+            value={email} 
+            onChange={(e: React.FormEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)} 
+          />
+          <input 
+            type="text" 
+            className={styles.contactInput} 
+            placeholder="Subject" 
+            value={subject} 
+            onChange={(e: React.FormEvent<HTMLInputElement>) => setSubject(e.currentTarget.value)} 
+          />
+          <textarea 
+            className={styles.contactTextarea} 
+            placeholder="Message" 
+            value={message} 
+            onChange={(e: React.FormEvent<HTMLTextAreaElement>) => setMessage(e.currentTarget.value)} 
+          />
+          <button 
+            type="button" 
+            onClick={sendEmail}
+            className={styles.contactBtn}
+          >
             Send message
           </button>
         </form>
