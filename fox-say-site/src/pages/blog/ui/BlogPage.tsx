14e4b563a9c7783a7b988c1660165e7171bb3c05
DimaChekashov <foxdima99@gmail.com>
2025-09-14 15:48:40 +0300

    add posts api

--- fox-say-site/src/pages/blog/ui/BlogPage.tsx
+++ fox-say-site/src/pages/blog/ui/BlogPage.tsx
@@ -1,7 +1,38 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+const fetchPosts = async () => {
+  const response = await fetch('/api/posts');
+  const posts = await response.json();
+  return posts;
+}
+
 export default function Blog() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const postData = await fetchPosts();
+				console.log(postData);
+				setPosts(postData);
+			} catch(err) {
+				console.error(err);
+			}
+		}
+
+		fetchData();
+	}, [])
+
 	return (
 		<div>
-				<h1>Blog</h1>
+			{posts.map((post) => (
+				<div key={post.id}>
+					<h3>{post.title}</h3>
+					<p>{post.content}</p>
+				</div>
+			))}
 		</div>
 	);
 }
