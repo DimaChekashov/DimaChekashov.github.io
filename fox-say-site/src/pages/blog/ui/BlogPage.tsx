c35239b5584bcb0f186f5ac8c6ad233de079826f
DimaChekashov <foxdima99@gmail.com>
2025-09-22 23:40:02 +0300

    Setup docker and add postgresql

--- fox-say-site/src/pages/blog/ui/BlogPage.tsx
+++ fox-say-site/src/pages/blog/ui/BlogPage.tsx
@@ -1,38 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-const fetchPosts = async () => {
-  const response = await fetch("/api/posts");
-  const posts = await response.json();
-  return posts;
-};
-
 export default function Blog() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const postData = await fetchPosts();
-        console.log(postData);
-        setPosts(postData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return <div>Blog</div>;
 }
