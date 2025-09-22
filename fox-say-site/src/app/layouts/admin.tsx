9d7a66fed1736ed9b925ab460eaad831493b2790
DimaChekashov <foxdima99@gmail.com>
2025-09-23 00:01:13 +0300

    Add admin panel route and page

---
+++ fox-say-site/src/app/layouts/admin.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+
+import "../styles/globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Foxsay Website - Admin Panel",
+  description: "admin panel",
+};
+
+export function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
