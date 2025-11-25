d45b61758982d7cb083d0f21f548b201e90df1fe
DimaChekashov <foxdima99@gmail.com>
2025-11-25 20:40:13 +0300

    Add posts on admin

--- fox-say-site/src/pages/admin-posts/ui/AdminPostsPage.tsx
+++ fox-say-site/src/pages/admin-posts/ui/AdminPostsPage.tsx
@@ -1,7 +1,15 @@
-export default function AdminPostsPage() {
+import { getPosts } from "@/shared/api/getPosts";
+import { IPost } from "@/shared/lib/types";
+
+export default async function AdminPostsPage() {
+  const posts = await getPosts();
+
   return (
     <div>
       <h1>Posts Page</h1>
+      {posts.data.map((post: IPost) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
     </div>
   );
 }
