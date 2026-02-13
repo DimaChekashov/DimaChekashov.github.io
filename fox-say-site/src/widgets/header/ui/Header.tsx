7ecf3ef0f01aef3b059b19898c563e93f66ff81e
DimaChekashov <foxdima99@gmail.com>
2026-02-14 01:34:25 +0300

    Add logo

--- fox-say-site/src/widgets/header/ui/Header.tsx
+++ fox-say-site/src/widgets/header/ui/Header.tsx
@@ -6,6 +6,9 @@ import { links } from "../model/consts";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+import logo from "@/shared/assets/images/logo.png";
 
 export const Header: React.FC = () => {
   const pathname = usePathname();
@@ -18,8 +21,8 @@ export const Header: React.FC = () => {
 
   return (
     <header className="mb-10 flex justify-between p-4 border-b border-gray-200">
-      <div>Fox Say Logo</div>
-      <nav>
+      <Image src={logo} alt="Logo" className="brightness-0 invert max-w-40" />
+      <nav className="flex items-center">
         <ul className="flex gap-4">
           {links.map(({ href, label }) => (
             <li key={href}>
@@ -27,7 +30,7 @@ export const Header: React.FC = () => {
                 href={href}
                 className={classNames(
                   "hover:text-blue-500",
-                  `/${firstSegment}` === href && "text-blue-500"
+                  `/${firstSegment}` === href && "text-blue-500",
                 )}
               >
                 {t(`Navbar.${label}`)}
