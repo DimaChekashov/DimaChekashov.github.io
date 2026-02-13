6bbfa460894ee2232bb21d98a5a816f274607694
DimaChekashov <foxdima99@gmail.com>
2026-02-14 02:19:10 +0300

    Add responsive for header

--- fox-say-site/src/pages/home/ui/HomePage.tsx
+++ fox-say-site/src/pages/home/ui/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/shared/ui/Button/Button";
 import { useTranslations } from "next-intl";
 
 export default function HomePage() {
@@ -5,7 +6,23 @@ export default function HomePage() {
 
   return (
     <div>
-      <h1>{t("title")}</h1>
+      <div>
+        <div>
+          <div className="w-40 h-40 bg-amber-400 rounded-xl"></div>
+        </div>
+        <div>
+          <h2>Привет, я Дмитрий Чекашов</h2>
+          <h3>
+            <span>Frontend</span> + <span>Backend</span> разработчик
+          </h3>
+          <p>Я разрабатываю красивые интерфейсы и производительные системы</p>
+          <div>
+            <Button>Контакты</Button>
+            <Button>Проекты</Button>
+          </div>
+        </div>
+      </div>
+      {/* <h1>{t("title")}</h1> */}
     </div>
   );
 }
--- fox-say-site/src/widgets/header/ui/Header.tsx
+++ fox-say-site/src/widgets/header/ui/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { links } from "../model/consts";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
@@ -9,21 +9,54 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 import logo from "@/shared/assets/images/logo.png";
+import { useIsClient, useMediaQuery, useScrollLock } from "usehooks-ts";
 
 export const Header: React.FC = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
   const pathname = usePathname();
   const t = useTranslations("Header");
+  const { lock, unlock } = useScrollLock();
+  const isClient = useIsClient();
+  const isMobile = useMediaQuery("(max-width: 767px)");
 
   const segments = pathname
     ? pathname.split("/").filter((segment) => segment !== "")
     : [];
   const firstSegment = segments[1] || "";
 
+  const handleToggleMenu = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
+
+  useEffect(() => {
+    if (isClient && isMobile && isOpenMenu) {
+      lock();
+    }
+
+    return () => {
+      unlock();
+    };
+  }, [isClient, isMobile, isOpenMenu, lock, unlock]);
+
+  const handleNavClick = () => {
+    if (isClient && !isMobile) return;
+    setIsOpenMenu(false);
+  };
+
   return (
     <header className="mb-10 flex justify-between p-4 border-b border-gray-200">
-      <Image src={logo} alt="Logo" className="brightness-0 invert max-w-40" />
-      <nav className="flex items-center">
-        <ul className="flex gap-4">
+      <Image
+        src={logo}
+        alt="Logo"
+        className="relative z-10 brightness-0 invert max-w-40"
+      />
+      <nav
+        className={classNames(
+          "bg-background fixed top-0 left-0 w-screen h-screen px-6 py-20 flex transition-all md:static md:p-0 md:w-auto md:h-auto md:items-center",
+          isOpenMenu ? "translate-x-0" : "translate-x-full",
+        )}
+      >
+        <ul className="flex flex-col gap-4 md:flex-row">
           {links.map(({ href, label }) => (
             <li key={href}>
               <Link
@@ -32,6 +65,7 @@ export const Header: React.FC = () => {
                   "hover:text-blue-500",
                   `/${firstSegment}` === href && "text-blue-500",
                 )}
+                onClick={handleNavClick}
               >
                 {t(`Navbar.${label}`)}
               </Link>
@@ -39,6 +73,14 @@ export const Header: React.FC = () => {
           ))}
         </ul>
       </nav>
+      <button
+        className="relative z-10 inline-flex flex-col gap-2 items-center justify-center w-10 h-10 md:hidden"
+        onClick={handleToggleMenu}
+      >
+        <span className="bg-white h-0.5 w-8" />
+        <span className="bg-white h-0.5 w-8" />
+        <span className="bg-white h-0.5 w-8" />
+      </button>
     </header>
   );
 };
